1fbb5a481d3c6ea61f1fbb826aeb2b6cf68d8dbd
Matteojk95 <matteopalmer2095@gmail.com>
2023-07-18 17:56:33 +0200

    done

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,19 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int base = scan.nextInt();
+        int altezza = scan.nextInt();
+
+        Rettangolo top = new Rettangolo(base, altezza);
+
+        System.out.println(top.calcoloarea());
+        System.out.println(top.calcoloperimetro());
+
+    }
 }
+
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,24 @@
-package org.lessons.java.geometria;public class Rettangolo {
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+
+    // CAMPI
+
+    int base;
+    int altezza;
+
+    // COSTRUTTORI
+    Rettangolo(int base, int altezza){
+        this.base= base;
+        this.altezza= altezza;
+    }
+
+    // METODI
+
+    int calcoloarea() {
+        return base*altezza;
+    }
+    int calcoloperimetro() {
+        return (base*2)+(altezza*2);
+    }
 }
